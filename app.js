12d8fc82c7f3495c3ed5274712790e1b5b763a96
Jean Paul Ruiz Arones <jpaul.ruiz.1802@gmail.com>
2025-03-05 13:10:27 -0500

    Realización del archivo JS y una interacción en css

---
+++ app.js
@@ -0,0 +1,5 @@
+const cuadro_descripcion=document.getElementById("cuadro-descripcion")
+cuadro_descripcion.innerHTML = `
+                                <h1>Rhagould Aksel</h1>
+                                <p>an innovative solar design practice that brings solar energy into daily life.</p>
+`
